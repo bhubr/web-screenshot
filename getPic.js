8852ff8a896f3149cdb8546faeb85268186f161f
Benoît Hubert <benoithubert@gmail.com>
2018-11-02 14:23:18 +0100

    Create dest dir if it doesn't exist

--- getPic.js
+++ getPic.js
@@ -1,7 +1,24 @@
 const puppeteer = require('puppeteer');
 const slugify = require('slugify');
+const mkdirp = require('mkdirp');
+const fs = require('fs');
+const { promisify } = require('util');
+const accessAsync = promisify(fs.access);
+const mkdirAsync = promisify(mkdirp);
+
+async function checkWritable(folder) {
+  await accessAsync(folder, fs.constants.W_OK);
+}
+
+async function mkdirIfNotExists (folder){
+  await checkWritable(folder)
+    .catch(err => mkdirAsync(folder));
+}
 
 async function getPic(url, destFolder) {
+  await mkdirIfNotExists(destFolder);
+
+  // OK, launch browser
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   const slug = slugify(url.replace(/https?\:\/\//, ''));
